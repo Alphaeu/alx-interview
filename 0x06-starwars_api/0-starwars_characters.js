8bb61b9630c28abce39907679fc9470393bd039b
Alphaeu <dealphaeus@gmail.com>
2024-06-06 23:08:46 +0300

    Corrections

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,54 +1,31 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const request = require('request');
 
-// Get the Movie ID from the command line arguments
 const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-if (!movieId) {
-    console.log('Please provide a Movie ID');
-    process.exit(1);
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
 }
 
-// The URL for the Star Wars API film endpoint
-const url = `https://swapi.dev/api/films/${movieId}/`;
+request(movieEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
 
-request(url, (error, response, body) => {
-    if (error) {
-        console.error('Error making request:', error);
-        return;
-    }
-
-    if (response.statusCode !== 200) {
-        console.error('Failed to retrieve data. Status code:', response.statusCode);
-        return;
-    }
-
-    // Parse the JSON response
-    const filmData = JSON.parse(body);
-
-    // Get the list of character URLs
-    const characterUrls = filmData.characters;
-
-    // For each character URL, make a request to get the character's name
-    characterUrls.forEach(characterUrl => {
-        request(characterUrl, (error, response, body) => {
-            if (error) {
-                console.error('Error making request:', error);
-                return;
-            }
-
-            if (response.statusCode !== 200) {
-                console.error('Failed to retrieve data. Status code:', response.statusCode);
-                return;
-            }
-
-            // Parse the JSON response
-            const characterData = JSON.parse(body);
-
-            // Print the character's name
-            console.log(characterData.name);
-        });
-    });
+    sendRequest(characterList, 0);
+  }
 });
-
