a84ff1928711ca76cb6e77caa98111650d88bf03
Alphaeu <dealphaeus@gmail.com>
2024-06-20 20:15:05 +0000

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """
-Interview Question on fewest number of coins needed to
+Interview Question on: fewest number of coins needed to
 meet a given amount total
 """
 
+
 def makeChange(coins, total):
+    """ fewest number of coins needed to meet total """
     if total <= 0:
         return 0
-    
-    """ Initialize DP array with a value larger than any possible number of coins"""
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: No coins needed to make 0 amount
-    
-    """ Process each amount from 1 to total"""
-    for amount in range(1, total + 1):
-        for coin in coins:
-            if amount - coin >= 0:
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    
-    """If dp[total] is still infinity, it means we cannot make the amount with given coins"""
-    return dp[total] if dp[total] != float('inf') else -1
+    # sort the coins in descending order
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
+        return -1
+    return change
